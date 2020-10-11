d22067452a80a328f62e6a90226a2c4efc9fcfa0
Saber Muthanna <34119145+SaberMuthanna@users.noreply.github.com>
2020-10-11 11:31:37 +0300

    project_upload

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/x', function () {
+    return view('x');
+});
 
 Auth::routes();
 
